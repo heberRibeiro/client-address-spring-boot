6204351bc641ef306757c309904d90b15d150367
Heber Ribeiro <heber.r@outlook.com>
2019-11-27 15:32:51 -0300

    Mapeamento JPA da entidade cliente

--- src/main/java/br/com/projects/main/entities/Cliente.java
+++ src/main/java/br/com/projects/main/entities/Cliente.java
@@ -3,10 +3,18 @@ package br.com.projects.main.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String cpf;
