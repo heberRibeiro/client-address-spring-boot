c7602f8783fb5aeb8090677bb1f219c17e2eb88c
Heber Ribeiro <heber.r@outlook.com>
2019-11-27 14:18:48 -0300

    Projeto iniciado

---
+++ src/main/java/br/com/projects/main/RestapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.projects.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiApplication.class, args);
+	}
+
+}
