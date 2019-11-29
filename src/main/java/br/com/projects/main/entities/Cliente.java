386a66fdfe84a71563533b66cc3ab3c09377c368
Heber Ribeiro <heber.r@outlook.com>
2019-11-29 20:45:59 -0300

    Anotação formato data

--- src/main/java/br/com/projects/main/entities/Cliente.java
+++ src/main/java/br/com/projects/main/entities/Cliente.java
@@ -11,6 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Entity
 @Table(name = "tb_cliente")
 public class Cliente implements Serializable {
@@ -26,6 +28,7 @@ public class Cliente implements Serializable {
 	@OneToOne(mappedBy = "cliente", cascade = CascadeType.ALL)
 	private Endereco endereco;
 	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataNascimento;
 
 	public Cliente() {
