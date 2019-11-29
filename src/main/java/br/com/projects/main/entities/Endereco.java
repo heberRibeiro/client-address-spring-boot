f7ed8ed5a51871efb6dae65e2ec0c63ba3ef42ef
Heber Ribeiro <heber.r@outlook.com>
2019-11-29 19:46:47 -0300

    Implements serializable da classe endereco

--- src/main/java/br/com/projects/main/entities/Endereco.java
+++ src/main/java/br/com/projects/main/entities/Endereco.java
@@ -1,5 +1,7 @@
 package br.com.projects.main.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,8 +13,10 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_endereco")
-public class Endereco {
+public class Endereco implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
